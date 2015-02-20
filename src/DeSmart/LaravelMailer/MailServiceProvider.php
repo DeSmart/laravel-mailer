a932d62a83c49c8d18f9c092003d199bf661c26e
Kamil Piechaczek <pomek@desmart.com>
2015-02-20 13:10:35 +0000

    FIX: fail build Travis - Only one argument is allowed per line in a multi-line function call

--- src/DeSmart/LaravelMailer/MailServiceProvider.php
+++ src/DeSmart/LaravelMailer/MailServiceProvider.php
@@ -26,9 +26,7 @@ class MailServiceProvider extends \Illuminate\Mail\MailServiceProvider
             // Once we have create the mailer instance, we will set a container instance
             // on the mailer. This allows us to resolve mailer classes via containers
             // for maximum testability on said classes instead of passing Closures.
-            $mailer = new Mailer(
-                $app['view'], $app['swift.mailer'], $app['events']
-            );
+            $mailer = new Mailer($app['view'], $app['swift.mailer'], $app['events']);
 
             $this->setMailerDependencies($mailer, $app);
 
