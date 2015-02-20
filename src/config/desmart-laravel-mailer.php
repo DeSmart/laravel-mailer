b3ab2ad70d1c51c4d174a7800aeb164c8b42a83e
Kamil Piechaczek <pomek@desmart.com>
2015-02-20 12:51:52 +0000

    Revert changes in config file

--- src/config/desmart-laravel-mailer.php
+++ src/config/desmart-laravel-mailer.php
@@ -10,7 +10,9 @@ return [
    | List of domains for which e-mail can be sent.
    |
    */
-  'white_list' => explode('|', env('DESMART_LARAVEL_MAILER_WHITE_LIST', '')),
+  'white_list' => [
+    'example.com',
+  ],
 
   /*
    |--------------------------------------------------------------------------
@@ -18,7 +20,7 @@ return [
    |--------------------------------------------------------------------------
    |
    */
-  'enabled' => env('DESMART_LARAVEL_MAILER_ENABLED', false),
+  'enabled' => false,
 
   /*
    |--------------------------------------------------------------------------
@@ -31,6 +33,6 @@ return [
    | All e-mails (which are not whitelisted) sent by Laravel will be sent to this address instead.
    |
    */
-  'email' => env('DESMART_LARAVEL_MAILER_EMAIL', '')
+  'email' => ''
 
 ];
