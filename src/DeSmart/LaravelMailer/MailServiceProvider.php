6e8035e78d2e90e4b8af9bb5d24379a27733ce9d
Kamil Piechaczek <pomek@desmart.com>
2015-02-20 13:07:20 +0000

    Upgrade ServiceProvider to L5

--- src/DeSmart/LaravelMailer/MailServiceProvider.php
+++ src/DeSmart/LaravelMailer/MailServiceProvider.php
@@ -7,9 +7,11 @@ class MailServiceProvider extends \Illuminate\Mail\MailServiceProvider
 
     public function register()
     {
-        $this->package('desmart/laravel-mailer');
+        $configPath = __DIR__ . '/../config/desmart-laravel-mailer.php';
+        $this->publishes([$configPath => config_path('desmart-laravel-mailer.php')], 'config');
+        $this->mergeConfigFrom($configPath, 'desmart-laravel-mailer');
 
-        if (true === $this->app['config']->get('laravel-mailer::mailer.enabled')) {
+        if (true === $this->app['config']->get('desmart-laravel-mailer.enabled')) {
             $this->registerMailer();
         } else {
             parent::register();
@@ -18,23 +20,34 @@ class MailServiceProvider extends \Illuminate\Mail\MailServiceProvider
 
     protected function registerMailer()
     {
-        $this->app->bindShared('mailer', function ($app) {
+        $this->app->singleton('mailer', function ($app) {
             $this->registerSwiftMailer();
 
-            $mailer = new Mailer($app['view'], $app['swift.mailer'], $app['events']);
-            $mailer->setLogger($app['log'])->setQueue($app['queue']);
-            $mailer->setContainer($app);
+            // Once we have create the mailer instance, we will set a container instance
+            // on the mailer. This allows us to resolve mailer classes via containers
+            // for maximum testability on said classes instead of passing Closures.
+            $mailer = new Mailer(
+                $app['view'], $app['swift.mailer'], $app['events']
+            );
 
+            $this->setMailerDependencies($mailer, $app);
+
+            // If a "from" address is set, we will set it on the mailer so that all mail
+            // messages sent by the applications will utilize the same "from" address
+            // on each one, which makes the developer's life a lot more convenient.
             $from = $app['config']['mail.from'];
 
-            if (is_array($from) and isset($from['address'])) {
+            if (is_array($from) && isset($from['address'])) {
                 $mailer->alwaysFrom($from['address'], $from['name']);
             }
 
+            // Here we will determine if the mailer should be in "pretend" mode for this
+            // environment, which will simply write out e-mail to the logs instead of
+            // sending it over the web, which is useful for local dev environments.
             $pretend = $app['config']->get('mail.pretend', false);
-            $mailer->pretend($pretend);
 
-            $mailer->setConfig($app['config']->get('laravel-mailer::mailer'));
+            $mailer->pretend($pretend);
+            $mailer->setConfig($app['config']->get('desmart-laravel-mailer'));
 
             return $mailer;
         });
